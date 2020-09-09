790e674a462e46700fcebf1fab2270ea931f3cac
Anupam Basak <anupam.basak27@gmail.com>
2020-09-09 18:14:47 +0530

    Improve deb build script

--- build-scripts/build-deb.sh
+++ build-scripts/build-deb.sh
@@ -5,12 +5,19 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 (
   mkdir -p $SCRIPTPATH/build
-  cd $SCRIPTPATH/build
   
-  cmake $SCRIPTPATH/.. -DCMAKE_INSTALL_PREFIX=/usr
-  make -j$(nproc)
+  pushd $SCRIPTPATH/build  
+    cmake $SCRIPTPATH/.. -DCMAKE_INSTALL_PREFIX=/usr
+    make -j$(nproc)
 
-  make -j$(nproc) package
-
+    make -j$(nproc) package
+    
+    source /etc/lsb-release
+    version=$(basename fingerboard*.deb | grep -Po '\d*\.\d*\.\d*')
+    arch=$(arch)
+    
+    mv fingerboard*.deb "${SCRIPTPATH}/fingerboard-${version}-$(echo ${DISTRIB_ID} | tr '[:upper:]' '[:lower:]')_${DISTRIB_CODENAME}-${arch}.deb"
+  popd
+  
   rm -rf $SCRIPTPATH/build
 )
